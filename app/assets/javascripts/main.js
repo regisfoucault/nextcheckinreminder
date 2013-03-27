cbb381912711fa43af2c08e2167e944fed7568e7
regisfoucault <regisfoucault@gmail.com>
2013-03-27 15:41:29 +0100

    Add: error management

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -3,44 +3,58 @@ $("#btn-search").click(function(e){
   var date = $("#search-input").attr("data-date");
   var searchInput = $("#search-input").val();
   var place = $(".custom-input-place").val();
-  $(".output").html('<i class="icon-spinner icon-spin"></i> Asking Foursquare ... ');
-  $.ajax({
-    type: "GET",
-    url: "https://api.foursquare.com/v2/venues/search?near=" + place + "&limit=20&query=" + searchInput + "&oauth_token=" + token + "&v=" + date,
-    success: function(s) {
-      console.log("success");
-      console.log(s.response.venues);
+  console.log(place);
+  if (place == "") {
+    $(".custom-input-place").addClass("error");
+    $(".custom-input-place").val("Where ?");
+  } else {
+    $(".output").html('<i class="icon-spinner icon-spin"></i> Asking Foursquare ... ');
+    $.ajax({
+      type: "GET",
+      url: "https://api.foursquare.com/v2/venues/search?near=" + place + "&limit=20&query=" + searchInput + "&oauth_token=" + token + "&v=" + date,
+      success: function(s) {
+        if (s.response.venues.length>0) {
+          var html = "<ul>";
+          _.each(s.response.venues, function(venue){
+            html += '<li data-id="' + venue.id + '">';
+              html += '<div class="row-fluid">';
+                html += '<div class="span3">';
+                  html += '<a href="' + venue.canonicalUrl + '">' + venue.name + '</a>';
+                html += '</div>';
+                html += '<div class="span6">' + venue.location.address + '</div>';
+                html += '<div class="span3">' + venue.location.city + '</div>';
+              html += '</div>';
+            html += '</li>';
+          })
+          html += "</ul>";
+          $(".output").html(html);
 
-      var html = "<ul>";
-      _.each(s.response.venues, function(venue){
-        html += '<li data-id="' + venue.id + '">';
-          html += '<div class="row-fluid">';
-            html += '<div class="span3">';
-              html += '<a href="' + venue.canonicalUrl + '">' + venue.name + '</a>';
-            html += '</div>';
-            html += '<div class="span6">' + venue.location.address + '</div>';
-            html += '<div class="span3">' + venue.location.city + '</div>';
-          html += '</div>';
-        html += '</li>';
-      })
-      html += "</ul>";
-      $(".output").html(html);
-
-      $(".output ul li").click(function(e) {
-        var elt = $($($(e.target).closest("li"))[0]);
-        if (elt.hasClass("selected")) {
-          elt.removeClass("selected");
+          $(".output ul li").click(function(e) {
+            var elt = $($($(e.target).closest("li"))[0]);
+            if (elt.hasClass("selected")) {
+              elt.removeClass("selected");
+            } else {
+              $(".output ul li").removeClass("selected");
+              elt.addClass("selected");
+            }
+          })
         } else {
-          $(".output ul li").removeClass("selected");
-          elt.addClass("selected");
+          $(".output").html("No venues found dude. Just create one !");
         }
-      })
-    },
-    error: function(e) {
-      console.log(e);
-    }
-  })
+      },
+      error: function(e) {
+        console.log(e);
+        $(".output").html("This place does not seem to be on earth !");
+        $(".custom-input-place").addClass("error");
+      }
+    })
+  }
 });
+
+$(".custom-input-place").focus(function(){
+  $(".custom-input-place").removeClass("error");
+})
+
 
 $(".sendTrigger").click(function(e){
   var vid = $($(".selected")[0]).attr("data-id");
